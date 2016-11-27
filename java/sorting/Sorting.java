0a06b1a4e89e915709cae5a65e392bb1ec1085c3
wangyaochong <wangyaochong>
2016-11-27 23:50:45 +0800

    新搭建的用于学习算法的环境

---
+++ java/sorting/Sorting.java
@@ -0,0 +1,7 @@
+package sorting;
+
+/**
+ * Created by wangy on 2016/11/27.
+ */
+public class Sorting {
+}
---
+++ java/sorting/TestSorting.java
@@ -0,0 +1,13 @@
+package sorting;
+
+import org.junit.Test;
+
+/**
+ * Created by wangy on 2016/11/27.
+ */
+public class TestSorting {
+    @Test
+    public void testXXXSorting() {
+
+    }
+}
