262e5c5d9354a78aa8113341eb0872fa4086b850
sx-9155 <sx-wangyc@dtdream.com>
2017-02-08 15:44:27 +0800

    完成两个算法。

---
+++ java/LintCode/LintCodeSolution/二进制中有多少个1.java
@@ -0,0 +1,33 @@
+package LintCode.LintCodeSolution;
+
+/**
+ * Created by 【王耀冲】 on 【2017/2/8】 at 【15:25】.
+ */
+
+import org.junit.Test;
+
+/**
+ * 计算在一个 32 位的整数的二进制表式中有多少个 1.
+ */
+public class 二进制中有多少个1 {
+    /**
+     * @param num: an integer
+     * @return: an integer, the number of ones in num
+     */
+    public int countOnes(int num) {
+        int oneCount=0;
+        int flag=1;
+        while(flag!=0){
+            oneCount+=(num&flag)==0?0:1;
+            //因为对于负数num，右移会导致高位填充1，
+            // 所以会产生死循环，因此使用标志位左移的方法找1的个数
+            flag=flag<<1;
+        }
+        return oneCount;
+    }
+    @Test
+    public void testCountOnes(){
+        int i = countOnes(-1);
+        System.out.println(i);
+    }
+}
---
+++ java/LintCode/LintCodeSolution/空格替换.java
@@ -0,0 +1,59 @@
+package LintCode.LintCodeSolution;
+
+/**
+ * Created by 【王耀冲】 on 【2017/2/8】 at 【9:30】.
+ */
+
+import org.junit.Test;
+
+/**
+ * 设计一种方法，将一个字符串中的所有空格替换成 %20 。
+ * 你可以假设该字符串有足够的空间来加入新的字符，且你得到的是“真实的”字符长度。
+ * 你的程序还需要返回被替换后的字符串的长度。
+ */
+public class 空格替换 {
+
+    /**
+     * @param string: An array of Char
+     * @param length: The true length of the string
+     * @return: The true length of new string
+     */
+
+    public int replaceBlank(char[] string, int length) {
+        if(string==null){
+            return 0;
+        }
+        int spaceCount=0;
+        int changedCount=0;
+        for (char c : string) {
+            if(c==' '){
+                spaceCount++;
+            }
+        }
+        int realLength=length+spaceCount*2;
+        int loc=length-1;
+        while(loc>=0&&spaceCount>0&&changedCount!=spaceCount){
+            if(string[loc]!=' '){
+                string[loc+(spaceCount-changedCount)*2]=string[loc];
+            }else{
+                string[loc+(spaceCount-changedCount)*2]='0';
+                string[loc+(spaceCount-changedCount)*2-1]='2';
+                string[loc+(spaceCount-changedCount)*2-2]='%';
+                changedCount=changedCount+1;
+            }
+            loc--;
+        }
+        return realLength;
+    }
+    @Test
+    public void testReplaceBlack(){
+        String test="abc abc ";
+        char [] testArray=new char[1000];
+        for (int i=0;i<test.length();i++){
+            testArray[i]=test.charAt(i);
+        }
+        int i = replaceBlank(testArray, test.length());
+        System.out.println(testArray);
+        System.out.println(i);
+    }
+}
