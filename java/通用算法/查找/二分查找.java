1905f8f13db0a9fe94c28ee32c64d2972c9c23bf
sx-9155 <sx-wangyc@dtdream.com>
2017-04-01 09:30:29 +0800

    修改二分查找的下标

--- java/通用算法/查找/二分查找.java
+++ java/通用算法/查找/二分查找.java
@@ -8,29 +8,32 @@ import org.junit.Test;
 public class 二分查找 {
     public int binarySearch(int [] nums,int target){
         int low=0;
-        int max=nums.length-1;
+        int high=nums.length-1;
 
         //这个地方需要注意，因为max-low有可能只相差1个数，那么除以二永远是0，
         // 则无法前进，因此需mid=low+(max-low+1)>>1
-        int mid=low+((max-low+1)>>1);
+        int mid=low+((high-low)>>1);
 
 
-        while(low<=max){
+        while(low<=high){
             if(nums[mid]<target){
                 low=mid+1;
             }else if(nums[mid]>target){
-                max=mid-1;
+                high=mid-1;
             }else{//如果相等，则返回
                 return mid;
             }
-            mid=low+((max-low)>>1);
+            mid=low+((high-low)>>1);
         }
         return -1;
     }
     @Test
     public void testBinarySearch(){
-        int [] nums=new int[]{-1,0,1,2,5};
-        System.out.println(binarySearch(nums,1));
+        int [] nums=new int[]{-1,0,1,2,4};
+        System.out.println(binarySearch(nums,-1));
         System.out.println(binarySearch(nums,0));
+        System.out.println(binarySearch(nums,1));
+        System.out.println(binarySearch(nums,2));
+        System.out.println(binarySearch(nums,4));
     }
 }
