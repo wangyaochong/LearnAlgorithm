4331d822cb240a0b3bab32daacdf12bb07728418
sx-9155 <sx-wangyc@dtdream.com>
2017-04-05 15:02:24 +0800

    改名

---
+++ java/LeetCode/LeetCodeSolution/S_001_TwoSum.java
@@ -0,0 +1,31 @@
+package LeetCode.LeetCodeSolution;
+
+import Util.UtilDisplay;
+import org.junit.Test;
+import java.util.HashMap;
+import java.util.Map;
+public class S_001_TwoSum {
+    //#1
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> tmp=new HashMap<Integer, Integer>();
+        Integer index;
+        for(int i=0;i<nums.length;i++){
+            int i1 = target - nums[i];
+            index=tmp.get(i1);
+            if(index==null){
+                tmp.put(nums[i],i);
+//                tmp.put(i,nums[i]);//注意，这种调用方式是错误的
+                //map.get()是根据key获取value，所以应该以值为key，获取下标为value
+            }else{
+                return new int[]{index,i};
+            }
+        }
+        return new int[]{1,1};
+    }
+    @Test
+    public void testTwoSum(){
+        int[] ints1 = {1, 2, 3, 4};
+        int[] ints = twoSum(ints1, 4);
+        UtilDisplay.display(ints);
+    }
+}
---
+++ java/LeetCode/LeetCodeSolution/S_032_LongestValidParentheses.java
@@ -0,0 +1,45 @@
+package LeetCode.LeetCodeSolution;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+public class S_032_LongestValidParentheses {
+    public int longestValidParentheses(String input){//()(())输出6
+        int max=0;
+        int current=0;
+        int lastValidPos=0;
+        int lastCurrent=0;
+        Stack<Character> cache=new Stack<Character>();
+        for(int i=0;i<input.length();i++){
+            if(input.charAt(i)=='('){
+                cache.push('(');
+            }
+            if(input.charAt(i)==')'){
+                if(cache.size()>0&& cache.peek()=='('){
+                    current+=2;
+                    cache.pop();
+                }
+                if(cache.size()==0) {//如果栈里面没有字符了，说明一轮连续的结束了
+                    if(i-lastValidPos==current){//如果是连续的，加上上一次的结果
+                        current=lastCurrent+current;
+                    }
+                    if(max<current){
+                        max=current;
+                    }
+                    lastValidPos=i;
+                    lastCurrent=current;
+                    current=0;
+                }
+                if(cache.size()!=0){
+
+                }
+            }
+        }
+        return max;
+    }
+    @Test
+    public void testLongestValidParentheses(){
+        System.out.println(longestValidParentheses("(()()(((((()()"));
+    }
+}
